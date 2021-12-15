fae7467da7f3ae6801dbfe3acd90ac9dfa440238
afeeniphillips <afeeni.phillips@bison.howard.edu>
2021-12-15 02:33:55 -0800

    counter project

---
+++ Counter/index.js
@@ -0,0 +1,26 @@
+
+// initialize count
+var count = 0
+
+
+// get button
+document.getElementById("button").onclick = function upCounter(){
+    document.getElementById("counter").innerText = count++
+}
+
+
+ 
+// function upCounter(){
+//     count+1
+// }
+// when button is clicked -> up the count by one
+
+console.log(count)
+
+
+// get counter id number
+document.getElementById("counter").innerText=count++
+// set counter to whater count is
+
+
+
