a05c3621a9896106d960a02e777e9c84a54ce9ad
afeeniphillips <afeeni.phillips@bison.howard.edu>
2021-12-15 03:58:45 -0800

    counter + practice folder

---
+++ Practice/functions.js
@@ -0,0 +1,27 @@
+function race(){
+
+
+}
+
+
+arrowFunction = () => {
+ }
+ 
+arrowFunction()
+
+// explicit arrow function
+arrowFunction => {
+    
+   return arrowFunction
+}
+
+
+// implicit arrow
+
+arrowFunction2 => (
+    arrowFunction2
+)
+
+
+///When a return statement is used in a function body, the execution of the function is stopped.
+//If the value is omitted, undefined is returned instead.
---
+++ Practice/index.js
@@ -0,0 +1,46 @@
+// function countdown(){
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+// countdown()
+// countdown()
+
+
+
+// const log42 = () => {
+//     console.log(42)
+//     return log42
+// }
+
+
+// function log42(){
+//     console.log(42)
+// }
+// log42()
+
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// const sum = () => {
+//     let totalTime = lap1 + lap2 + lap3
+//     console.log(totalTime)
+// }
+
+// sum()
+
+
+let lapsCompleted = 0
+
+const inc = () => {
+    lapsCompleted++
+}
+inc()
+inc()
+inc()
+console.log(lapsCompleted)
