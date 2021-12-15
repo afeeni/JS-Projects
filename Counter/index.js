38b63c0439cd85af07f695623b10b62515d05281
afeeniphillips <afeeni.phillips@bison.howard.edu>
2021-12-15 04:09:38 -0800

    counter + practice folder

--- Counter/index.js
+++ Counter/index.js
@@ -1,26 +1,12 @@
 
 // initialize count
-var count = 0
 
+let count = 0
 
 // get button
-document.getElementById("button").onclick = function upCounter(){
-    document.getElementById("counter").innerText = count++
+function increment(){
+    count++
+    console.log("the button was clicked") + console.log(count)
+    document.getElementById("counter").innerText = count
 }
-
-
- 
-// function upCounter(){
-//     count+1
-// }
-// when button is clicked -> up the count by one
-
-console.log(count)
-
-
-// get counter id number
-document.getElementById("counter").innerText=count++
-// set counter to whater count is
-
-
 
